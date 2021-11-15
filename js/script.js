d45034c6780d303dff85f9bde8dc1bd8b0180166
nicolebaggerly <nkbaggerly@gmail.com>
2021-11-15 13:28:42 -0700

    Part 2 - Fetch and display repos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const profileOverview = document.querySelector(".overview"); //Div where profile info will appera
 const username = "nicolebaggerly"; //Github username
+const displayRepoList = document.querySelector(".repo-list"); //UL where repos will be displayed
 
+//Fetches Github user info 
 const profileInfo = async function () {
     const res = await fetch (`https://api.github.com/users/${username}`);
     const data = await res.json();
@@ -9,6 +11,7 @@ const profileInfo = async function () {
 };
 profileInfo();
 
+//Creates new div and displays fetched info 
 const displayInfo = function (data) {
     const userInfo = document.createElement("div");
     userInfo.className = "user-info";
@@ -22,4 +25,23 @@ const displayInfo = function (data) {
           <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
         </div> `;
     profileOverview.append(userInfo);
+    repos();
+};
+
+
+//Fetches repos 
+const repos = async function () {
+    const repoRes = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await repoRes.json();
+    displayRepos(repoData);
+};
+
+
+const displayRepos = function (repos) {
+    for (let repo of repos) {
+        const item = document.createElement("li");
+        item.classList.add("repo");
+        item.innerHTML = `<h3>${repo.name}</h3>`; //Use repo (no S) because looping through each indiv. repo
+        displayRepoList.append(item);
+    }
 }
